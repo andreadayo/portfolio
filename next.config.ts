0a6e15b1c336e03941e1e402f7de1748c96aa3b9
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2026-08-15 23:36:33 +0800

    build: install sass

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: ["./src/styles", "./styles"],
+  },
 };
 
 export default nextConfig;
