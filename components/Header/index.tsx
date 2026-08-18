bff19b6a69a979a2db77628c87ad585b37f70516
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2026-08-19 02:49:21 +0800

    fix(font): font-weight adjustments

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,21 @@
+import Link from "next/link";
 import Container from "@/components/Container";
 import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Header() {
   return (
     <Container smallPadding>
-      <span
-        style={{ fontSize: "var(--text-sm)", color: "var(--text-secondary)" }}
-      >
-        AD
-      </span>
+      <Link href="/">
+        <span
+          style={{
+            fontFamily: "var(--font-azeret-mono)",
+            fontSize: "var(--text-sm)",
+            color: "var(--text-secondary)",
+          }}
+        >
+          AD
+        </span>
+      </Link>
       <ThemeToggle />
     </Container>
   );
