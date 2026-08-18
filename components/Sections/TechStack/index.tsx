d8656e86fab61f15cadf127d1d572ddd5d40e91a
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2026-08-18 22:50:25 +0800

    feat(tech-stack): add tech-stack section

--- components/Sections/TechStack/index.tsx
+++ components/Sections/TechStack/index.tsx
@@ -1,9 +1,31 @@
+import styles from "./styles.module.scss";
 import Container from "@/components/Container";
+import SectionHeader from "@/components/SectionHeader";
 
 export default function TechStack() {
   return (
-    <Container fillHeight>
-      <span>tech stack</span>
+    <Container>
+      <div className={styles.techstack}>
+        <SectionHeader headerIcon="tech-stack" headerLabel="Tech Stack" />
+
+        <div className={styles.techList}>
+          <div className={styles.techItem}>
+            <h2 className={styles.title}>Web</h2>
+            <span className={styles.subtitle}>
+              JavaScript, TypeScript, React, Next.js, Bootstrap, Tailwind CSS,
+              Motion (Framer), GSAP, Node.js, PHP, SQL
+            </span>
+          </div>
+
+          <div className={styles.techItem}>
+            <h2 className={styles.title}>Web</h2>
+            <span className={styles.subtitle}>
+              JavaScript, TypeScript, React, Next.js, Bootstrap, Tailwind CSS,
+              Motion (Framer), GSAP, Node.js, PHP, SQL
+            </span>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
