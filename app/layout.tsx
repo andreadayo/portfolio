fd5cfa8597cd2c5893c8d30be7db8f9e63d5c830
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2026-08-19 02:44:01 +0800

    refactor(font): change main font to general sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,30 +6,30 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.scss";
 
-const neueMontreal = localFont({
+const generalSans = localFont({
   src: [
     {
-      path: "../public/fonts/NeueMontreal-Light.otf",
+      path: "../public/fonts/GeneralSans-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../public/fonts/NeueMontreal-Regular.otf",
+      path: "../public/fonts/GeneralSans-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../public/fonts/NeueMontreal-Medium.otf",
+      path: "../public/fonts/GeneralSans-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../public/fonts/NeueMontreal-Bold.otf",
+      path: "../public/fonts/GeneralSans-Bold.ttf",
       weight: "700",
       style: "normal",
     },
   ],
-  variable: "--font-neue-montreal",
+  variable: "--font-general-sans",
 });
 
 const azeretMono = Azeret_Mono({
@@ -46,7 +46,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${neueMontreal.variable} ${azeretMono.variable}`}
+      className={`${generalSans.variable} ${azeretMono.variable}`}
       suppressHydrationWarning
     >
       <body>
